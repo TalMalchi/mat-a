3ec7e1e448473d1303b37d4e93d88ebb21905850
TalMalchi <malchital@gmail.com>
2022-03-08 18:32:15 +0200

    Update mat.cpp

--- mat.cpp
+++ mat.cpp
@@ -7,9 +7,14 @@ using namespace std;
 
 std::string ariel::mat(int a, int b, char c, char d){
     
-    if (a%2==0 || b%2==0 || a<0 || b<0){
-        throw invalid_argument("number must odd");
+    if (a%2==0 || b%2==0 ){
+        throw invalid_argument("number must odd ");
     }
+    if (a<0 || b<0)
+    {
+       throw invalid_argument("number must positive");
+    }
+    
      return "a";
     
     };
