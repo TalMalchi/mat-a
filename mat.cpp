7f569f136d77c2840ce7d5e8fb5d889af2384768
TalMalchi <malchital@gmail.com>
2022-03-08 18:13:52 +0200

    last changes

---
+++ mat.cpp
@@ -0,0 +1,16 @@
+#include "mat.hpp"
+using namespace ariel;
+
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+std::string ariel::mat(int a, int b, char c, char d){
+    
+    if (a%2==0 || b%2==0 || a<0 || b<0){
+        throw invalid_argument("number must odd");
+    }
+     return "a";
+    
+    };
+
---
+++ mat.hpp
@@ -0,0 +1,19 @@
+
+#include <string>
+#ifndef CPP_MAT_H
+#define CPP_MAT_H
+#endif //CPP_MAT_H
+
+namespace ariel
+{
+std::string mat(int a, int b, char c, char d);
+} // namespace ariel
+
+
+// std::string nospaces(std::string input) {
+// 	std::erase(input, ' ');
+// 	std::erase(input, '\t');
+// 	std::erase(input, '\n');
+// 	std::erase(input, '\r');
+// 	return input;
+// };
